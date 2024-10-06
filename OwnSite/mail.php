eeb5c39f100bf805e91e9d5fd6493e6aecc915cb
crymoooreee <info@cry-more.online>
2024-10-07 00:30:31 +0500

    Bug fixes

--- OwnSite/mail.php
+++ OwnSite/mail.php
@@ -1,25 +1,26 @@
 <?php
-$to = "info@cry-more.online"; // емайл получателя данных из формы 
-$tema = "Форма обратной связи"; // тема полученного емайла 
+$to = "info@cry-more.online"; // email of the recipient of the form data
+$tema = "Форма обратной связи"; // subject of the received email
 
-// Проверка наличия данных в $_POST
-if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message'])) {
-    $message = "Имя отправителя: ".$_POST['name']."<br>";
-    $message .= "Почта: ".$_POST['email']."<br>";
-    $message .= "Номер телефона: ".$_POST['phone']."<br>";
-    $message .= "Сообщение: ".$_POST['message']."<br>";
+// Check if all fields are filled in the $_POST
+if (isset($_POST['email']) && isset($_POST['name']) && isset($_POST['phone']) && isset($_POST['message'])) {
+    $message = "Email address: ".$_POST['email']."<br>";
+    $message .= "Your Name: ".$_POST['name']."<br>";
+    $message .= "Phone Number: ".$_POST['phone']."<br>";
+    $message .= "Message: ".$_POST['message']."<br>";
 
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
-    // Отправка письма
+    // Send the email
     if (mail($to, $tema, $message, $headers)) {
         echo '<script>alert("Письмо отправлено успешно!")</script>';
-    } else {
-        echo '<script>alert("Ошибка отправки письма!")</script>';
-
-    }
-} else {
-    echo '<script>alert("Не все поля заполнены!")</script>';
-}
+    } else { 
+        echo '<script>alert("Ошибка отправки письма!")</script>'; 
+    } 
+    } 
+    else { 
+        echo '<script>alert("Не все поля заполнены!")</script>'; 
+    } 
+    
 ?>
